c97d40d5adef331ceaa7d13bc7e8e7cd1f13da7c
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2020-12-18 10:23:49 +0100

    feat: helm 3.4.2

--- release.sh
+++ release.sh
@@ -4,7 +4,8 @@
 # export BUILD_VER=3.4.2
 export BUILD_VER=$1
 sed -i -r -e "s/^(ENV VERSION=)(.*)*/\1${BUILD_VER}/" Dockerfile
-docker build . || exit 99
+docker build --tag datenbetrieb/helm:$BUILD_VER . || exit 99
 git add Dockerfile
 git commit -m "feat: helm $BUILD_VER"
 git tag -m "helm $BUILD_VER" $BUILD_VER
+docker push datenbetrieb/helm:$BUILD_VER
